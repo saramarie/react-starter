d231ecd8a64819de22356da32cfefe8bd0544169
Sara Williams <admin@truthyfalsey.com>
2017-06-11 10:53:52 -0400

    Fixing up Sass a little

--- js/components/App.js
+++ js/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+require('../../scss/components/app.scss');
+
 export default class App extends React.Component {
   render () {
     return (
--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
-require('../scss/styles.scss');
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
