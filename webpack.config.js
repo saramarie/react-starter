bedd6a5af7b833e9b477ff58eaa5d25d30c76cd9
Sara Williams <sarawilliams@Saras-MacBook-Pro.local>
2017-06-11 09:15:47 -0400

    Basic React dev setup with Webpack, Babel and JSX support

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
