748e3f95de92622ae32ee0490bc35fe2eda2e786
Sara Williams <admin@truthyfalsey.com>
2017-10-24 14:21:02 -0400

    Putting class and its export on different lines

--- js/components/App.js
+++ js/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './app.scss';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render () {
     return (
       <div>
@@ -11,3 +11,5 @@ export default class App extends React.Component {
     )
   }
 }
+
+export default App;
